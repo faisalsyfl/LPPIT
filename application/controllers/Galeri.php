3ca371162064e04e4a1d2449cfe5d800a3947969
faisalsyfl <faisalsyfl@gmail.com>
2017-10-19 13:09:08 +0700

    edit Data

--- application/controllers/Galeri.php
+++ application/controllers/Galeri.php
@@ -3,9 +3,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Galeri extends CI_Controller {
 
+	private $data;
+	public function __construct(){
+		parent::__construct();
+		$this->data['allp'] = $this->ModelPelatihan->selectAll(6)->result_array();		
+
+	}
     public function index()	{
 		// var_dump($data['all']);
-		$this->load->view('templates/header');
+		$this->load->view('templates/header',$this->data);
 		$this->load->view('galeri');
 		$this->load->view('templates/footer');
 	}

bcdd7f03eeec720ea74392bb3fc3e10664fcaac5
trisamsul <trisamsul@gmail.com>
2017-10-30 13:44:13 +0700

    nambah foto pelatihan

--- application/views/pelatihan.php
+++ application/views/pelatihan.php
@@ -15,13 +15,13 @@
 <main class="site-main page-main">
     <div class="container">
         <div class="row">
-            <section class="page col-sm-7">
+            <section class="page col-sm-8">
                 <h2 class="page-title"><?php echo "Pelatihan ".$training['nama']; ?></h2>
                 <div class="entry text-justify">
                     <?php echo $training['keterangan']; ?>                        
                 </div>
             </section>
-            <aside class="sidebar col-sm-5">
+            <aside class="sidebar col-sm-4">
                 <div class="widget">
                     <h4>Info Pelatihan</h4>
                     <table style="margin-left: 15px;">
@@ -54,6 +54,33 @@
                     </table>
                 </div>
             </aside>
+        </div>
+        <div class="row">
+            <div class="col-lg-8 col-md-8 contact-wthree2">
+                <h2 class="category-title">Foto Kegiatan Pelatihan</h2>
+                <div class="row">
+                    <div class="col-md-3 col-sm-6" style="margin-bottom: 20px;">
+                        <div class="text-center">
+                            <a href="<?php echo base_url(); ?>assets/img/h1.jpeg" class="zoom"><img src="<?php echo base_url(); ?>assets/img/h1.jpeg" class="img-responsive rounded" alt="..."></a>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6" style="margin-bottom: 20px;">
+                        <div class="text-center">
+                            <a href="<?php echo base_url(); ?>assets/img/h2.jpg" class="zoom"><img src="<?php echo base_url(); ?>assets/img/h2.jpg" class="img-responsive rounded" alt="..."></a>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6" style="margin-bottom: 20px;">
+                        <div class="text-center">
+                            <a href="<?php echo base_url(); ?>assets/img/h3.jpeg" class="zoom"><img src="<?php echo base_url(); ?>assets/img/h3.jpeg" class="img-responsive rounded" alt="..."></a>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6" style="margin-bottom: 20px;">
+                        <div class="text-center">
+                            <a href="<?php echo base_url(); ?>assets/img/h4.jpeg" class="zoom"><img src="<?php echo base_url(); ?>assets/img/h4.jpeg" class="img-responsive rounded" alt="..."></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </main>
